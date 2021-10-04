36c592794b99aed56d0f79605094226d6b7cf42d
Andreas Larssamils <larssamils.andreas@gmail.com>
2021-10-04 23:46:02 +0800

    add it back

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,12 @@ class RootIndex extends React.Component {
   render() {
     const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
     const [author] = get(this, 'props.data.allContentfulPerson.nodes')
-    const andreas = 'Andreas Larssamils'
 
     return (
       <Layout location={this.props.location}>
         <Hero
           image={author.heroImage.gatsbyImageData}
-          title={andreas}
+          title={author.name}
           content={author.shortBio.shortBio}
         />
         <ArticlePreview posts={posts} />
